898f5d2c6ed3f3bfad749f5a49924850b5513533
epassarelli <epassarelli@gmail.com>
2019-09-23 00:12:14 -0300

    Panel - Ver un administrado

---
+++ application/modules/mipanel/views/misadministrados_mostrar_view.php
@@ -0,0 +1,34 @@
+<!-- Default box -->
+<div class="box">
+  
+  <div class="box-header with-border">
+    <h3 class="box-title"><?php echo $title; ?></h3>
+
+    <div class="box-tools pull-right">
+      <a class="btn btn-default" href="<?php echo base_url();?>mipanel/interpretes" role="button"><i class="fa fa-reply"></i> Volver a Mis Administrados</a>
+      <a class="btn btn-success" href="<?php echo base_url();?>mipanel/interpretes/nuevo" role="button"><i class="fa fa-plus"></i> Nuevo interprete</a>
+
+      <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+              title="Collapse">
+        <i class="fa fa-minus"></i></button>
+      <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+        <i class="fa fa-times"></i></button>
+    </div>
+  </div>
+  
+  <div class="box-body">
+
+    
+  
+
+
+  </div>
+  <!-- /.box-body -->
+  
+  <div class="box-footer">
+    Footer
+  </div>
+  <!-- /.box-footer-->
+
+</div>
+<!-- /.box -->
