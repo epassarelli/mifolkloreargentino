70539e3ab573c23e50f5b105421e93801c88ea8f
Yohan Fuenmayor <yohanfuenmayorm@gmail.com>
2019-12-26 14:41:35 -0300

    Metodo insertar show

--- application/modules/mipanel/controllers/Shows.php
+++ application/modules/mipanel/controllers/Shows.php
@@ -4,11 +4,14 @@ class Shows extends MX_Controller {
 
 function __construct(){
 	parent::__construct();
-	if (!$this->tank_auth->is_logged_in()){
-		redirect('/auth/login/');
-	} 
+
+	// if (!$this->tank_auth->is_logged_in()){
+	// 	redirect('/auth/login/');
+	// } 
+
 	$this->load->model('Shows_model');
 	$_SESSION['seccion'] = "Shows";
+
 	if (ENVIRONMENT == 'development') {
 		$this->output->enable_profiler(TRUE);
 	}
@@ -47,6 +50,79 @@ public function index(){
 	$data['view']       	= 'misshows_view';
 	//$data['sidebar']       	= 'interpretes_sidebar_view';
 	$this->load->view('layout.php', $data);
+}
+
+####################################################
+###			Nuevo Show del Artista
+####################################################		
+
+public function nuevo(){
+	$data['title'] = "Nuevo show folklorico";
+	
+	$this->form_validation->set_rules('titulo', 'titulo', 'required|trim|min_length[5]');
+	$this->form_validation->set_rules('fecha', 'fecha', 'required');
+	$this->form_validation->set_rules('hora', 'hora', 'required');
+	$this->form_validation->set_rules('provincia', 'provincia', 'required');
+	$this->form_validation->set_rules('localidad', 'localidad', 'required');
+	$this->form_validation->set_rules('lugar', 'lugar', 'required|trim|min_length[5]');
+	$this->form_validation->set_rules('direccion', 'direccion', 'required|trim|min_length[5]');
+	$this->form_validation->set_rules('detalle', 'detalle', 'required|trim|min_length[5]');
+	
+	
+	$this->form_validation->set_message('required', 'Debe introducir el campo "%s"');
+	$this->form_validation->set_message('min_length', 'El campo "%s" debe ser de al menos %s carcteres');	
+	
+	// Si no pasó la validacion
+	if($this->form_validation->run()==FALSE)
+	{	
+			$this->load->model('admin/Provincias_model');
+			$data['provincias'] 	= $this->Provincias_model->get_all();
+
+			$data['breadcrumb'] = array(
+							'Inicio' => base_url()
+						);
+								
+			$data['view'] 			= "misshows_form_view";
+			$data['accion'] 		= "insertar";
+			$this->load->view('layout', $data);	
+	}
+	else
+		{
+			$evento['even_titulo'] 		= $this->input->post('titulo');
+			$evento['even_fecha'] 		= $this->input->post('fecha');
+			$evento['even_hora'] 		= $this->input->post('hora');
+			$evento['prov_id'] 			= $this->input->post('provincia');
+			$evento['loca_id'] 			= $this->input->post('localidad');
+			$evento['even_lugar'] 		= $this->input->post('lugar');
+			$evento['even_direccion'] 	= $this->input->post('direccion');
+			$evento['even_detalle'] 	= $this->input->post('detalle');
+			$evento['inte_id'] 			= $this->tank_auth->get_user_inte_id();
+			
+			// inserto el show	
+			if ($this->Shows_model->insertar($evento)){	
+			
+				// if( $_SERVER['SERVER_NAME'] != 'localhost' ) {
+				// 	// Mando un correo a los administradores
+				// 	$this->load->library('email');
+				// 	$this->email->from('info@mifolkloreargentino.com.ar', 'Mi Folklore Argentino');
+				// 	$this->email->to('epassarelli@gmail.com', 'aruffo73@gmail.com');
+				// 	$this->email->subject('Datos de Artista actualizados');
+				// 	$mensaje = "Sugirieron un evento: " . $this->input->post('titulo') . "<br />";
+				// 	$mensaje .= "para el dia: " . $this->input->post('fecha') . "<br /><br />";
+				// 	$mensaje .= nl2br($this->input->post('detalle'));
+				// 	$this->email->message($mensaje);
+				// 	$this->email->send();
+				// }
+				
+				$this->session->set_flashdata('mensaje', 'ok');
+			}
+			else{
+				$this->session->set_flashdata('mensaje', 'error');
+			}
+
+			$redirecta = base_url() . "mipanel/misshows";
+			Header("Location: $redirecta"); 
+		}
 }
 
 }
